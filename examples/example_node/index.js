4c3bd4efd352316249b4600cf12bf3a9070eff37
Paul Smith <prsmith@ranken.edu>
2020-05-11 10:51:22 -0500

    serving static files from the public dir

--- examples/example_node/index.js
+++ examples/example_node/index.js
@@ -1,13 +1,50 @@
 const debug = require('debug')('app:startup');
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const hostname = process.env.HOSTNAME || 'localhost';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((request, response) => {
+  console.log(`${request.method} ${request.url}`);
+
+  const url = new URL(request.url, `http://${request.headers.host}`);
+  switch (url.pathname) {
+    case '/': {
+      // Home Page
+      serveStaticFile(response, '/index.html');
+      break;
+    }
+    default: {
+      // Serve static files from public dir
+      serveStaticFile(response, url.pathname);
+      break;
+    }
+  }
 });
 
-const PORT = process.env.PORT;
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function serveStaticFile(response, url) {
+  const filename = __dirname + '/public' + url;
+  fs.exists(filename, (exists) => {
+    if (!exists) {
+      response.statusCode = 404;
+      response.setHeader('Content-Type', 'text/plain');
+      response.end("file not found");
+    } else {
+      fs.readFile(filename, (err, data) => {
+        if (err) {
+          response.statusCode = 404;
+          response.setHeader('Content-Type', 'application/json');
+          response.end(JSON.stringify(err));
+        } else {
+          response.statusCode = 200;
+          response.end(data);
+        }
+      });
+    }
+  });
+}
