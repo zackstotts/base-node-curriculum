d5162b3b4ab39011466c3e971cf257bbacc7fa88
Paul Smith <prsmith@ranken.edu>
2020-05-11 10:08:39 -0500

    basic web server

--- examples/example_node/index.js
+++ examples/example_node/index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const debug = require('debug')('app:startup');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+const PORT = process.env.PORT;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
