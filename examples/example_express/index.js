779af633c0fb018cc9e10c73f5136be05c8c4e6b
Paul Smith <prsmith@ranken.edu>
2020-05-11 12:50:50 -0500

    convert example website to express.js

--- examples/example_express/index.js
+++ examples/example_express/index.js
@@ -1,76 +1,41 @@
-const debugServer = require('debug')('app:server');
-const debugFiles = require('debug')('app:files');
-const http = require('http');
-const fs = require('fs');
-const querystring = require('querystring');
+const debug = require('debug')('app:server');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
 
 const hostname = process.env.HOSTNAME || 'localhost';
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-  debugServer(`${request.method} ${request.url}`);
+const app = express();
 
-  const url = new URL(request.url, `http://${request.headers.host}`);
-  switch (url.pathname) {
-    case '/': {
-      // Home Page
-      serveStaticFile(response, '/index.html');
-      break;
-    }
-    case '/contact': {
-      // Contact Form
-      if (request.method == 'POST') {
-        let body = '';
-        request.on('data', (chunk) => {
-          body += chunk.toString();
-        });
-        request.on('end', () => {
-          debugServer(body);
-          const params = querystring.parse(body);
-          debugServer('Message Received!');
-          debugServer(`\temail: ${params.email}`);
-          debugServer(`\tsubject: ${params.subject}`);
-          debugServer(`\tmessage: ${params.message}`);
-          serveStaticFile(response, '/contact_sent.html');
-        });
-      } else {
-        serveStaticFile(response, '/contact.html');
-      }
-      break;
-    }
-    default: {
-      // Otherwise, serve static files from public dir
-      serveStaticFile(response, url.pathname);
-      break;
-    }
-  }
+// Middleware
+app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public/index.html'));
+});
+app.get('/contact', (request, response) => {
+  response.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+app.post('/contact', (request, response) => {
+  const params = request.body;
+
+  debug('Message Received!');
+  debug(`\temail: ${params.email}`);
+  debug(`\tsubject: ${params.subject}`);
+  debug(`\tmessage: ${params.message}`);
+
+  response.sendFile(path.join(__dirname, 'public/contact_sent.html'));
 });
 
-server.listen(port, hostname, () => {
-  debugServer(`Server running at http://${hostname}:${port}/`);
+app.use(express.static('public'));
+app.use((request, response) => {
+  response.status(404).type('text/plain').send('Page Not Found');
 });
 
-function serveStaticFile(response, url) {
-  const filename = __dirname + '/public' + url;
-  fs.exists(filename, (exists) => {
-    if (!exists) {
-      response.statusCode = 404;
-      response.setHeader('Content-Type', 'text/plain');
-      response.end('FILE NOT FOUND');
-      debugFiles(`FILE NOT FOUND ${filename}`);
-    } else {
-      fs.readFile(filename, (err, data) => {
-        if (err) {
-          response.statusCode = 404;
-          response.setHeader('Content-Type', 'application/json');
-          response.end(JSON.stringify(err));
-          debugFiles(`ERROR ${filename}`);
-        } else {
-          response.statusCode = 200;
-          response.end(data);
-          //debugFiles(`SERVED ${filename}`);
-        }
-      });
-    }
-  });
-}
+// Bind the server to an http port
+app.listen(port, hostname, () => {
+  debug(`Server running at http://${hostname}:${port}/`);
+});
