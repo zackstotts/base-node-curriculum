3f2d54df9856de8d9ec4771fab717286d7798ab8
Paul Smith <prsmith@ranken.edu>
2020-05-14 08:10:47 -0500

    logging the inputs even if they aren't all completed

--- unit_00/examples/example_bootstrap/js/main.js
+++ unit_00/examples/example_bootstrap/js/main.js
@@ -7,11 +7,13 @@ $('#Contact-form').submit((e) => {
   const email = formData.get('email');
   const subject = formData.get('subject');
   const message = formData.get('message');
+
+  console.log('Message Received!');
+  console.log(`\temail: ${email}`);
+  console.log(`\tsubject: ${subject}`);
+  console.log(`\tmessage: ${message}`);
+
   if (email && subject && message) {
-    console.log('Message Received!');
-    console.log(`\temail: ${email}`);
-    console.log(`\tsubject: ${subject}`);
-    console.log(`\tmessage: ${message}`);
     $(e.target).replaceWith('<h4>Message has been sent.</h4>');
   } else {
     $('#Contact-output').html('<h4 class="text-danger">Please complete all fields.</h4>');
