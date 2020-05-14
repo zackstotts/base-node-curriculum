d9e4be79ea6b11fdcdf3c3191ed3e52486684c71
Paul Smith <prsmith@ranken.edu>
2020-05-14 18:22:21 -0500

    adding a script to pull all example projects from their source repos

---
+++ pull-all-subtrees.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git subtree pull -P unit_02/examples/notepad_mysql notepad-mysql master --squash
+git subtree pull -P unit_03/examples/notepad_mongodb notepad-mongodb master --squash
+git subtree pull -P unit_06/examples/notepad_mongodb_react notepad-react master --squash
+
