4014a6e7d85aa4d58166f3e7829366af2e9f20c5
Paul Smith <prsmith@ranken.edu>
2020-05-14 08:03:52 -0500

    using jquery to handle the form submit event

---
+++ unit_00/examples/example_bootstrap/js/main.js
@@ -0,0 +1,19 @@
+// https://api.jquery.com/submit/
+// https://developer.mozilla.org/en-US/docs/Web/API/FormData
+
+$('#Contact-form').submit((e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const email = formData.get('email');
+  const subject = formData.get('subject');
+  const message = formData.get('message');
+  if (email && subject && message) {
+    console.log('Message Received!');
+    console.log(`\temail: ${email}`);
+    console.log(`\tsubject: ${subject}`);
+    console.log(`\tmessage: ${message}`);
+    $(e.target).replaceWith('<h4>Message has been sent.</h4>');
+  } else {
+    $('#Contact-output').html('<h4 class="text-danger">Please complete all fields.</h4>');
+  }
+});
