cd61565e0de33e18f12aa31ee36d1266a45f0243
Paul Smith <prsmith@ranken.edu>
2020-05-15 07:33:05 -0500

    unsquashing commits on merge

--- pull-all-subtrees.sh
+++ pull-all-subtrees.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git subtree pull -P unit_02/examples/notepad_mysql notepad-mysql master --squash
-git subtree pull -P unit_03/examples/notepad_mongodb notepad-mongodb master --squash
-git subtree pull -P unit_06/examples/notepad_mongodb_react notepad-react master --squash
-
+git subtree pull -P unit_02/examples/notepad_mysql notepad-mysql master
+git subtree pull -P unit_03/examples/notepad_mongodb notepad-mongodb master
+git subtree pull -P unit_06/examples/notepad_mongodb_react notepad-react master
