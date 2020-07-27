184224ef5112219c2af4f0ab786be125830bdf2a
Paul Smith <prsmith@ranken.edu>
2020-07-27 14:22:32 -0500

    downloading unit 3 slides

---
+++ unit_03/slides/mongo_db_example/insert_Multiple_Records.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = [
+    { name: 'Jeff', address: '755 Parr Rd.'},
+    { name: 'Paul', address: '4431 Finney Ave.'},
+    { name: 'Evan', address: '4431 Finney Ave'},
+  ];
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    db.close();
+  });
+});
