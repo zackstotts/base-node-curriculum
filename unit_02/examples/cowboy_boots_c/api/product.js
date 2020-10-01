c08425fa6845111e2270dcba840e28a243b0ad16
Paul Smith <prsmith@ranken.edu>
2020-10-01 16:15:51 -0500

    building a REST API

---
+++ unit_02/examples/cowboy_boots_c/api/product.js
@@ -0,0 +1,80 @@
+const db = require('../db');
+const express = require('express');
+const debug = require('debug')('app:api:product');
+
+const router = express.Router();
+router.use(express.urlencoded());
+router.use(express.json());
+
+router.get('/', async (req, res, next) => {
+  debug('get all');
+  try {
+    const products = await db.getAllProducts();
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/id/:id', async (req, res, next) => {
+  debug('find by id');
+  try {
+    const id = req.params.id;
+    const product = await db.findProductById(id);
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/name/:name', async (req, res, next) => {
+  debug('find by name');
+  try {
+    const name = req.params.name;
+    const products = await db.findProductByName(name);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+router.get('/category/:category', async (req, res, next) => {
+  debug('find by category');
+  try {
+    const category = req.params.category;
+    const products = await db.findProductByCategory(category);
+    res.json(products);
+  } catch (err) {
+    next(err);
+  }
+});
+router.post('/', async (req, res, next) => {
+  debug('insert product');
+  try {
+    const product = req.body;
+    const result = await db.insertProduct(product);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+router.put('/:id', async (req, res, next) => {
+  debug('update product');
+  try {
+    const product = req.body;
+    product.id = req.params.id;
+    const result = await db.updateProduct(product);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+router.delete('/:id', async (req, res, next) => {
+  debug('delete product');
+  try {
+    const id = req.params.id;
+    const result = await db.deleteProduct(id);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- unit_02/examples/cowboy_boots_c/server.js
+++ unit_02/examples/cowboy_boots_c/server.js
@@ -5,15 +5,8 @@ require('dotenv').config();
 const debug = require('debug')('app:server');
 const express = require('express');
 const hbs = require('express-handlebars');
-const mysql = require('mysql');
 const config = require('config');
-
-const databaseConfig = config.get('db');
-const knex = require('knex')({
-  client: 'mysql',
-  connection: databaseConfig,
-});
-//const pool = mysql.createPool(databaseConfig);
+const db = require('./db');
 
 // create and configure application
 const app = express();
@@ -30,51 +23,29 @@ app.use(express.urlencoded({ extended: false }));
 
 // routes
 app.get('/', (req, res, next) => {
-  knex('products')
-    .select('*')
-    .orderBy('name')
+  db.getAllProducts()
     .then((results) => {
       res.render('home', { title: 'Home Page', products: results });
     })
     .catch((err) => {
       next(err);
     });
-
-  // pool.query('SELECT * FROM products', (err, results) => {
-  //   if (err) {
-  //     next(err);
-  //   } else {
-  //     res.render('home', { title: 'Home Page', products: results });
-  //   }
-  // });
 });
 app.get('/product/:id', (req, res, next) => {
   const id = req.params.id;
 
-  knex('products')
-    .select('*')
-    .where('id', id)
-    .then((results) => {
-      if (results && results.length == 1) {
-        const product = results[0];
+  db.findProductById(id)
+    .then(product => {
+      if (product) {
         res.render('product-view', { title: product.name, product });
       } else {
         res.status(404).type('text/plain').send('product not found');
       }
     })
     .catch((err) => next(err));
-
-  // pool.query(`SELECT * FROM products WHERE id = '${id}'`, (err, results) => {
-  //   if (err) {
-  //     next(err);
-  //   } else if (results.length == 1) {
-  //     const product = results[0];
-  //     res.render('product-view', { title: product.name, product });
-  //   } else {
-  //     res.status(404).send('Product Not Found');
-  //   }
-  // });
 });
+
+app.use('/api/product', require('./api/product'));
 
 // static files
 app.use('/', express.static('public'));
