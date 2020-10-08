b4d3b22844d71763bcc451f4a01c960e4547dbce
Paul Smith <prsmith@ranken.edu>
2020-10-08 08:44:25 -0500

    starting part f

---
+++ unit_02/examples/cowboy_boots_f/routes/product.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const db = require('../db');
+//const debug = require('debug')('app:routes:product');
+
+const router = express.Router();
+
+// eslint-disable-next-line no-unused-vars
+router.get('/', (req, res, next) => {
+  db.getAllProducts()
+    .then((results) => {
+      res.render('product-list', { title: 'Product List', products: results });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// eslint-disable-next-line no-unused-vars
+router.get('/add', (req, res, next) => {
+  res.render('product-add', { title: 'Add Product' });
+});
+
+router.get('/edit/:id', (req, res, next) => {
+  const id = req.params.id;
+  db.findProductById(id)
+    .then((product) => {
+      if (product) {
+        res.render('product-edit', { title: 'Edit Product', product });
+      } else {
+        res.status(404).type('text/plain').send('product not found');
+      }
+    })
+    .catch((err) => next(err));
+});
+
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  db.findProductById(id)
+    .then((product) => {
+      if (product) {
+        res.render('product-view', { title: product.name, product });
+      } else {
+        res.status(404).type('text/plain').send('product not found');
+      }
+    })
+    .catch((err) => next(err));
+});
+
+module.exports = router;
---
+++ unit_02/examples/cowboy_boots_f/server.js
@@ -0,0 +1,36 @@
+// initialize environment variables
+require('dotenv').config();
+
+// import libraries
+const debug = require('debug')('app:server');
+const express = require('express');
+const hbs = require('express-handlebars');
+const config = require('config');
+//const db = require('./db');
+
+// create and configure application
+const app = express();
+app.engine(
+  'handlebars',
+  hbs({
+    helpers: {
+      formatPrice: (price) => '$' + price.toFixed(2),
+    },
+  })
+);
+app.set('view engine', 'handlebars');
+//app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use('/product', require('./routes/product'));
+app.use('/api/product', require('./api/product'));
+
+// static files
+app.use('/', express.static('public'));
+
+// start app
+const hostname = config.get('http.hostname');
+const port = config.get('http.port');
+app.listen(port, () => {
+  debug(`Server running at http://${hostname}:${port}/`);
+});
