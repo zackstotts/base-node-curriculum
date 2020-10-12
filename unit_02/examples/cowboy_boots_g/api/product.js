af462cd2b5fa717aa59683f67fa901237c636348
Paul Smith <prsmith@ranken.edu>
2020-10-12 09:06:49 -0500

    starting part g

---
+++ unit_02/examples/cowboy_boots_g/api/product.js
@@ -0,0 +1,126 @@
+const db = require('../db');
+const express = require('express');
+const debug = require('debug')('app:api:product');
+const Joi = require('joi');
+
+const router = express.Router();
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+
+const sendError = (err, res) => {
+  debug(err);
+  if (err.isJoi) {
+    res.json({ error: err.details.map((x) => x.message).join('\n') });
+  } else {
+    res.json({ error: err.message });
+  }
+};
+
+// eslint-disable-next-line no-unused-vars
+router.get('/', async (req, res, next) => {
+  debug('get all');
+  try {
+    const products = await db.getAllProducts();
+    res.json(products);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.get('/id/:id', async (req, res, next) => {
+  debug('find by id');
+  try {
+    const schema = Joi.number().min(1).required();
+    const id = await schema.validateAsync(req.params.id);
+    const product = await db.findProductById(id);
+    res.json(product);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.get('/name/:name', async (req, res, next) => {
+  debug('find by name');
+  try {
+    const schema = Joi.string().required().trim();
+    const name = await schema.validateAsync(req.params.name);
+    const products = await db.findProductByName(name);
+    res.json(products);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.get('/category/:category', async (req, res, next) => {
+  debug('find by category');
+  try {
+    const schema = Joi.string().required().trim();
+    const category = await schema.validateAsync(req.params.category);
+    const products = await db.findProductByCategory(category);
+    res.json(products);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.post('/', async (req, res, next) => {
+  debug('insert product');
+  try {
+    const schema = Joi.object({
+      name: Joi.string().required().min(3).max(100).trim(),
+      category: Joi.string().required().min(3).max(7).trim(),
+      price: Joi.number().required().min(0).max(9999.99).precision(2),
+    });
+    const product = await schema.validateAsync(req.body, { abortEarly: false });
+    const result = await db.insertProduct(product);
+    res.json(result);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.put('/:id', async (req, res, next) => {
+  debug('update product');
+  try {
+    const schema = Joi.object({
+      id: Joi.number().required().min(1),
+      name: Joi.string().required().min(3).max(100).trim(),
+      category: Joi.string().required().min(3).max(7).trim(),
+      price: Joi.number().required().min(0).max(9999.99).precision(2),
+    });
+    let product = req.body;
+    product.id = req.params.id;
+    product = await schema.validateAsync(product, { abortEarly: false });
+    const result = await db.updateProduct(product);
+    res.json(result);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+router.delete('/:id', async (req, res, next) => {
+  debug('delete product');
+  try {
+    const schema = Joi.number().min(1).required();
+    const id = await schema.validateAsync(req.params.id);
+    const result = await db.deleteProduct(id);
+    res.json(result);
+  } catch (err) {
+    //next(err);
+    sendError(err, res);
+  }
+});
+
+module.exports = router;
---
+++ unit_02/examples/cowboy_boots_g/public/js/main.js
@@ -0,0 +1,108 @@
+$(() => {
+  $('form.needs-validation').on('submit', (evt) => {
+    if (!evt.target.checkValidity()) {
+      evt.preventDefault();
+      evt.stopPropagation();
+    }
+    $(evt.target).addClass('was-validated');
+  });
+  // $('#add-product').click((evt) => {
+  //   const name = prompt('what is the name?');
+  //   const category = prompt('what is category?');
+  //   const price = prompt('what is price?');
+
+  //   $.ajax({
+  //     method: 'POST',
+  //     url: '/api/product',
+  //     dataType: 'json',
+  //     data: { name, category, price }
+  //   }).done((res) => {
+  //     //alert(res[0]);
+  //     window.location = new URL(`/product/${res[0]}`, window.location);
+  //   }).fail((xhr, textStatus, err) => {
+  //     alert(`${textStatus}\n${err}`);
+  //   });
+  // });
+  $('#add-product-form').on('submit', (evt) => {
+    evt.preventDefault();
+
+    $(evt.target).addClass('was-validated');
+    if (!evt.target.checkValidity()) {
+      return;
+    }
+
+    // const name = $('#name').val();
+    // const category = $('#category').val();
+    // const price = $('#price').val();
+    const formData = $('#add-product-form').serialize();
+    console.log(formData);
+
+    $.ajax({
+      method: 'POST',
+      url: '/api/product',
+      dataType: 'json',
+      data: formData,
+    })
+      .done((res) => {
+        if (res.error) {
+          $('#add-product-form output').html(res.error);
+        } else {
+          window.location = new URL(`/product/${res[0]}`, window.location);
+        }
+      })
+      .fail((xhr, textStatus, err) => {
+        $('#add-product-form output').html(`${textStatus}\n${err}`);
+      });
+  });
+  $('#edit-product-form').on('submit', (evt) => {
+    evt.preventDefault();
+
+    $(evt.target).addClass('was-validated');
+    if (!evt.target.checkValidity()) {
+      return;
+    }
+
+    const id = $('#id').val();
+    const formData = $(evt.target).serialize();
+    console.log(formData);
+
+    $.ajax({
+      method: 'PUT',
+      url: `/api/product/${id}`,
+      dataType: 'json',
+      data: formData,
+    })
+      .done((res) => {
+        if (res.error) {
+          $('#edit-product-form output').html(res.error);
+        } else {
+          window.location = new URL(`/product/${id}`, window.location);
+        }
+      })
+      .fail((xhr, textStatus, err) => {
+        $('#edit-product-form output').html(`${textStatus}\n${err}`);
+      });
+  });
+  $('.delete-product-from-list').on('click', (evt) => {
+    const id = $(evt.target).data('id');
+    const name = $(evt.target).data('name');
+    const yes = confirm(`Are you sure that you want to delete "${name}"`);
+    if (yes) {
+      $.ajax({
+        method: 'DELETE',
+        url: `/api/product/${id}`,
+        dataType: 'json',
+      })
+        .done((res) => {
+          if (res.error) {
+            alert(res.error);
+          } else {
+            window.location.reload();
+          }
+        })
+        .fail((xhr, textStatus, err) => {
+          alert(`${textStatus}\n${err}`);
+        });
+    }
+  });
+});
