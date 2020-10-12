546afad1f3f04d4b520ebc898e72354f8a41f59e
Paul Smith <prsmith@ranken.edu>
2020-10-12 10:08:12 -0500

    moving product views into a folder

--- unit_02/examples/cowboy_boots_g/routes/product.js
+++ unit_02/examples/cowboy_boots_g/routes/product.js
@@ -2,13 +2,14 @@ const express = require('express');
 const db = require('../db');
 //const debug = require('debug')('app:routes:product');
 
+// create and configure router
 const router = express.Router();
 
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   db.getAllProducts()
     .then((results) => {
-      res.render('product-list', { title: 'Product List', products: results });
+      res.render('product/list', { title: 'Product List', products: results });
     })
     .catch((err) => {
       next(err);
@@ -17,7 +18,7 @@ router.get('/', (req, res, next) => {
 
 // eslint-disable-next-line no-unused-vars
 router.get('/add', (req, res, next) => {
-  res.render('product-add', { title: 'Add Product' });
+  res.render('product/add', { title: 'Add Product' });
 });
 
 router.get('/edit/:id', (req, res, next) => {
@@ -25,7 +26,7 @@ router.get('/edit/:id', (req, res, next) => {
   db.findProductById(id)
     .then((product) => {
       if (product) {
-        res.render('product-edit', { title: 'Edit Product', product });
+        res.render('product/edit', { title: 'Edit Product', product });
       } else {
         res.status(404).type('text/plain').send('product not found');
       }
@@ -39,7 +40,7 @@ router.get('/:id', (req, res, next) => {
   db.findProductById(id)
     .then((product) => {
       if (product) {
-        res.render('product-view', { title: product.name, product });
+        res.render('product/view', { title: product.name, product });
       } else {
         res.status(404).type('text/plain').send('product not found');
       }
@@ -47,4 +48,5 @@ router.get('/:id', (req, res, next) => {
     .catch((err) => next(err));
 });
 
+// export router
 module.exports = router;
