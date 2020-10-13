4d2905f82c16a8d7a30a1745a602d5fbc53287d2
Paul Smith <prsmith@ranken.edu>
2020-10-13 10:31:47 -0500

    converting to async await

--- unit_02/examples/cowboy_boots_g/routes/product.js
+++ unit_02/examples/cowboy_boots_g/routes/product.js
@@ -6,12 +6,13 @@ const db = require('../db');
 const router = express.Router();
 
 // eslint-disable-next-line no-unused-vars
-router.get('/', (req, res, next) => {
-  db.getAllProducts()
-    .then((results) => {
-      res.render('product/list', { title: 'Product List', products: results });
-    })
-    .catch((err) => next(err));
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await db.getAllProducts();
+    res.render('product/list', { title: 'Product List', products });
+  } catch (err) {
+    next(err);
+  }
 });
 
 // eslint-disable-next-line no-unused-vars
@@ -19,31 +20,32 @@ router.get('/add', (req, res, next) => {
   res.render('product/add', { title: 'Add Product' });
 });
 
-router.get('/edit/:id', (req, res, next) => {
-  const id = req.params.id;
-  db.findProductById(id)
-    .then((product) => {
-      if (product) {
-        res.render('product/edit', { title: 'Edit Product', product });
-      } else {
-        res.status(404).render('error/basic', { title: 'Product Not Found' });
-      }
-    })
-    .catch((err) => next(err));
+router.get('/edit/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const product = await db.findProductById(id);
+    if (product) {
+      res.render('product/edit', { title: 'Edit Product', product });
+    } else {
+      res.status(404).render('error/basic', { title: 'Product Not Found' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  db.findProductById(id)
-    .then((product) => {
-      if (product) {
-        res.render('product/view', { title: product.name, product });
-      } else {
-        res.status(404).render('error/basic', { title: 'Product Not Found' });
-      }
-    })
-    .catch((err) => next(err));
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const product = await db.findProductById(id);
+    if (product) {
+      res.render('product/view', { title: product.name, product });
+    } else {
+      res.status(404).render('error/basic', { title: 'Product Not Found' });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 // export router
